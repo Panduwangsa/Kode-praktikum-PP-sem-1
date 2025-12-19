8a21d521f32e0914252020a48119386851a26fff
Panduwangsa <Panduwangsa28@gmail.com>
2025-12-19 23:09:42 +0700

    Create Pert11-writingFile.cpp

---
+++ Pert11-writingFile.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    fstream myFile;
+    
+    // Membuka file dalam mode output (tulis)
+    myFile.open("data.txt", ios::out);
+    
+    // Menulis teks ke dalam file
+    myFile << "wenno sivan";
+    
+    myFile.close();
+    return 0;
+}
